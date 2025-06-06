2209d7c9639fc9d7362d3406e6edefa52c64b952
vinicius-ssantos <viniciusolicveira@gmail.com>
2025-06-06 01:37:49 -0300

    Initialize Spring Boot project with Maven setup and core application structure.

---
+++ src/main/java/io/deviceid/create_ia_profiles/CreateIaProfilesApplication.java
@@ -0,0 +1,13 @@
+package io.deviceid.create_ia_profiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateIaProfilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreateIaProfilesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/deviceid/create_ia_profiles/CreateIaProfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.deviceid.create_ia_profiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateIaProfilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
