4c9441d8e3a1cd1f99cfb45bca16fa090801546a
vinicius-ssantos <viniciusolicveira@gmail.com>
2025-06-07 11:32:15 -0300

    Enable BackupProperties configuration in CreateIaProfilesApplication

--- src/main/java/com/example/matchapp/CreateIaProfilesApplication.java
+++ src/main/java/com/example/matchapp/CreateIaProfilesApplication.java
@@ -1,12 +1,13 @@
 package com.example.matchapp;
 
+import com.example.matchapp.config.BackupProperties;
 import com.example.matchapp.config.ImageGenProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(scanBasePackages = {"com.example.matchapp"})
-@EnableConfigurationProperties(ImageGenProperties.class)
+@EnableConfigurationProperties({ImageGenProperties.class, BackupProperties.class})
 public class CreateIaProfilesApplication {
 
     public static void main(String[] args) {
